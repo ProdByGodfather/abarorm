05ab4c39746b580baf4ce1aded7947c3238faf4d
ProdByGodfather <mahdighasemiabari84@gmail.com>
2025-01-13 15:26:47 +0330

    update __init__ file

--- abarorm/__init__.py
+++ abarorm/__init__.py
@@ -1,7 +1,19 @@
+"""
+Abarorm Package Initialization
+
+This file imports the necessary database models to be exposed publicly.
+The following models are available:
+    - SQLiteModel
+    - PostgreSQLModel
+"""
+
 __all__ = [
     'SQLiteModel',
     'PostgreSQLModel'
 ]
 
-from .sqlite import SQLiteModel
-from .psql import PostgreSQLModel
+try:
+    from .sqlite import SQLiteModel
+    from .psql import PostgreSQLModel
+except ImportError as e:
+    raise ImportError(f"Error importing module: {e}")
